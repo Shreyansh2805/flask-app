7a46d6f34355b7c968fac4729f28793bb557ce84
Shreyansh2805 <110284410+Shreyansh2805@users.noreply.github.com>
2026-06-13 22:55:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import flask
+app = Flask(__name__)
 
+@app.route('/')
+def hello world ():
+    return "Hello. Welcome to my First Python DevOps Project"
+
+@app.route('/health')
+def health:
+    return 'Server is Up and Running'
